e9fad7df758927feb98af30cafe20833ebd8fb41
yangkaige <1546865912@qq.com>
2020-03-27 15:43:54 +0800

    first commit

---
+++ pages/post/_id.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="container">
+    post item
+    <p>
+      {{postId}}
+    </p>
+  </div>
+</template>
+
+<script>
+import Logo from "~/components/Logo.vue";
+
+export default {
+  head() {
+    return {
+      title: "Post Page",
+      meta: [{name:'twiter',content:'fewregregreg'}]
+    };
+  },
+  data() {
+    return {
+      postId: this.$route.params.id
+    };
+  }
+};
+</script>
+
+<style>
+</style>
